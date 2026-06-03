007647a6fd599610ddbab1df94d17ac87f70418a
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-03 00:05:57 -0400

    gitupload: ALink.h

--- ALink.h
+++ ALink.h
@@ -1,27 +1,35 @@
 #pragma once
 #include "ILink.h"
-#include "RingBuffer.h"
+#include <vector>
 
 enum State { OK, SWP, LCK };
 
 class ALink {
     ILink* hw;
     bool isMaster;
-    volatile State state;
-    volatile int errs;
-    volatile int spdI;
-    int spds[5] = {9600, 19200, 38400, 57600, 115200};
-    volatile int scores[5];
-    RingBuffer rxBuf;
+    
+    // Protected by hardware lock()
+    State state;
+    int errs;
+    int spdI;
+    std::vector<int> scores;
+    
+    // Configuration
+    std::vector<int> spds;
+    int errThreshold;
+    int timerDelayMs;
 
 public:
-    ALink(ILink* hw, bool isMasterNode);
+    ALink(ILink* hw, bool isMasterNode, 
+          std::vector<int> allowedBauds = {9600, 19200, 38400, 57600, 115200}, 
+          int errorThreshold = 5, 
+          int delayMs = 50);
     
     void err(); 
     int available();
     int read(uint8_t* b, int max_len);
     void write(const uint8_t* b, int len);
-    State getState() { return state; }
+    State getState();
     
     void onRx(const uint8_t* data, int len);
     void onBreak();
