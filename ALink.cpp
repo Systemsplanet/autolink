7d9d034f217e213150f6ee75127d00f2612d70ad
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-03 00:05:55 -0400

    gitupload: ALink.cpp

--- ALink.cpp
+++ ALink.cpp
@@ -1,69 +1,107 @@
 #include "ALink.h"
 
-ALink::ALink(ILink* h, bool isMasterNode) {
+ALink::ALink(ILink* h, bool isMasterNode, std::vector<int> allowedBauds, int errorThreshold, int delayMs) {
     hw = h;
     isMaster = isMasterNode;
+    spds = allowedBauds;
+    errThreshold = errorThreshold;
+    timerDelayMs = delayMs;
+    
     state = OK;
     errs = 0;
     spdI = 0;
+    scores.resize(spds.size(), 0);
+    
     hw->bind(this);
-    for(int i=0; i<5; i++) scores[i]=0;
 }
 
 void ALink::err() {
-    if (state != OK) return;
+    hw->lock();
+    if (state != OK) {
+        hw->unlock();
+        return;
+    }
     errs++;
-    if (errs > 5) {
+    bool trigger = (errs > errThreshold);
+    hw->unlock();
+
+    if (trigger) {
         hw->sendBreak();
         onBreak(); 
     }
 }
 
 int ALink::available() { 
-    return rxBuf.available(); 
+    return hw->appBufAvailable(); 
 }
 
 int ALink::read(uint8_t* b, int max_len) {
     int n = 0;
-    while(n < max_len && rxBuf.available()) {
-        b[n++] = (uint8_t)rxBuf.pop();
+    while(n < max_len && hw->appBufAvailable() > 0) {
+        int val = hw->popAppBuf();
+        if(val >= 0) b[n++] = (uint8_t)val;
     }
     return n;
 }
 
 void ALink::write(const uint8_t* b, int len) {
-    if (state == OK) { 
+    hw->lock();
+    State s = state;
+    hw->unlock();
+    
+    if (s == OK) { 
         hw->tx(b, len); 
     }
+}
+
+State ALink::getState() { 
+    hw->lock();
+    State s = state;
+    hw->unlock();
+    return s;
 }
 
 void ALink::onRx(const uint8_t* data, int len) {
     for(int i=0; i<len; i++) {
         uint8_t b = data[i];
         
-        if (state == OK) {
-            rxBuf.push(b);
+        hw->lock();
+        State s = state;
+        hw->unlock();
+        
+        if (s == OK) {
+            hw->pushAppBuf(b);
         } 
-        else if (state == SWP) {
-            if (b == 0x55 && spdI < 5) scores[spdI]++;
+        else if (s == SWP) {
+            hw->lock();
+            if (b == 0x55 && spdI < (int)spds.size()) scores[spdI]++;
+            hw->unlock();
         } 
-        else if (state == LCK) {
+        else if (s == LCK) {
             if (isMaster) {
-                if (b < 5) { 
+                if (b < spds.size()) { 
                     hw->setSpd(spds[b]);
+                    hw->lock();
                     state = OK; errs = 0;
+                    hw->unlock();
                 }
             } else {
                 if (b == 0xAA) { 
                     int best = 0;
-                    for(int j=1; j<5; j++) {
+                    hw->lock();
+                    for(size_t j=1; j<spds.size(); j++) {
                         if (scores[j] >= scores[best]) best = j;
                     }
+                    hw->unlock();
+                    
                     uint8_t res = best;
                     hw->tx(&res, 1);
                     hw->flushTx();
                     hw->setSpd(spds[best]);
+                    
+                    hw->lock();
                     state = OK; errs = 0;
+                    hw->unlock();
                 }
             }
         }
@@ -71,33 +109,48 @@ void ALink::onRx(const uint8_t* data, int len) {
 }
 
 void ALink::onBreak() {
+    hw->lock();
     state = SWP;
     spdI = 0;
-    rxBuf.clear();
-    for(int i=0; i<5; i++) scores[i]=0;
+    for(size_t i=0; i<scores.size(); i++) scores[i]=0;
+    hw->unlock();
+    
+    hw->clearAppBuf();
     hw->setSpd(spds[0]);
-    hw->startTimer(50); 
+    hw->startTimer(timerDelayMs); 
 }
 
 void ALink::onTimer() {
-    if (state == SWP) {
-        if (isMaster && spdI < 5) {
+    hw->lock();
+    State s = state;
+    int curSpd = spdI;
+    hw->unlock();
+    
+    if (s == SWP) {
+        if (isMaster && curSpd < (int)spds.size()) {
             uint8_t ping = 0x55;
             hw->tx(&ping, 1);
             hw->flushTx();
         }
         
+        hw->lock();
         spdI++;
-        if (spdI < 5) {
-            hw->setSpd(spds[spdI]);
-            hw->startTimer(50);
+        curSpd = spdI;
+        hw->unlock();
+        
+        if (curSpd < (int)spds.size()) {
+            hw->setSpd(spds[curSpd]);
+            hw->startTimer(timerDelayMs);
         } else {
+            hw->lock();
             state = LCK;
+            hw->unlock();
+            
             hw->setSpd(9600);
-            if (isMaster) hw->startTimer(50); 
+            if (isMaster) hw->startTimer(timerDelayMs); 
         }
     } 
-    else if (state == LCK && isMaster) {
+    else if (s == LCK && isMaster) {
         uint8_t req = 0xAA;
         hw->tx(&req, 1);
         hw->flushTx();
