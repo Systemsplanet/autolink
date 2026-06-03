987862ea19d582db59bc492b25b6f9e6e75e0326
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-03 00:05:59 -0400

    gitupload: EspHal.h

--- EspHal.h
+++ EspHal.h
@@ -3,20 +3,24 @@
 #include "ALink.h"
 #include <Arduino.h>
 #include "driver/uart.h"
+#include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
+#include "freertos/stream_buffer.h"
 
 class EspHal : public ILink {
     uart_port_t uart_num;
     QueueHandle_t uart_queue;
     TaskHandle_t task_handle;
     TimerHandle_t timer_handle;
+    StreamBufferHandle_t stream_buf;
+    portMUX_TYPE mux;
 
     static void uart_event_task(void *pvParameters) {
         EspHal* hal = (EspHal*)pvParameters;
         uart_event_t event;
-        uint8_t* dtmp = (uint8_t*) malloc(256);
+        uint8_t dtmp[256]; // Stack allocation replaces malloc
         
         while(1) {
             if(xQueueReceive(hal->uart_queue, (void * )&event, (TickType_t)portMAX_DELAY)) {
@@ -30,7 +34,6 @@ class EspHal : public ILink {
                 }
             }
         }
-        free(dtmp);
     }
 
     static void timer_callback(TimerHandle_t xTimer) {
@@ -40,6 +43,9 @@ class EspHal : public ILink {
 
 public:
     EspHal(uart_port_t u_num, int rx_pin, int tx_pin) : uart_num(u_num) {
+        mux = portMUX_INITIALIZER_UNLOCKED;
+        stream_buf = xStreamBufferCreate(1024, 1);
+        
         uart_config_t uart_config = {
             .baud_rate = 9600,
             .data_bits = UART_DATA_8_BITS,
@@ -48,9 +54,10 @@ public:
             .flow_ctrl = UART_HW_FLOWCTRL_DISABLE
         };
         
-        uart_driver_install(uart_num, 512, 512, 10, &uart_queue, 0);
-        uart_param_config(uart_num, &uart_config);
-        uart_set_pin(uart_num, tx_pin, rx_pin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE);
+        // Wrapped in ESP_ERROR_CHECK for error catching
+        ESP_ERROR_CHECK(uart_driver_install(uart_num, 512, 512, 10, &uart_queue, 0));
+        ESP_ERROR_CHECK(uart_param_config(uart_num, &uart_config));
+        ESP_ERROR_CHECK(uart_set_pin(uart_num, tx_pin, rx_pin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
         
         xTaskCreate(uart_event_task, "uart_ev_task", 2048, this, 12, &task_handle);
         timer_handle = xTimerCreate("alink_tmr", pdMS_TO_TICKS(50), pdFALSE, this, timer_callback);
@@ -60,15 +67,38 @@ public:
         vTaskDelete(task_handle);
         xTimerDelete(timer_handle, 0);
         uart_driver_delete(uart_num);
+        vStreamBufferDelete(stream_buf);
     }
 
     void setSpd(int spd) override { uart_set_baudrate(uart_num, spd); }
     void sendBreak() override { uart_write_bytes_with_break(uart_num, "\0", 1, 15); }
     void tx(const uint8_t* b, int n) override { uart_write_bytes(uart_num, (const char*)b, n); }
     void flushTx() override { uart_wait_tx_done(uart_num, portMAX_DELAY); }
+    
     void startTimer(int ms) override {
         xTimerChangePeriod(timer_handle, pdMS_TO_TICKS(ms), 0);
         xTimerStart(timer_handle, 0);
     }
     void stopTimer() override { xTimerStop(timer_handle, 0); }
+    
+    void lock() override { portENTER_CRITICAL(&mux); }
+    void unlock() override { portEXIT_CRITICAL(&mux); }
+    
+    void pushAppBuf(uint8_t b) override {
+        xStreamBufferSend(stream_buf, &b, 1, 0);
+    }
+    
+    int popAppBuf() override {
+        uint8_t b;
+        if (xStreamBufferReceive(stream_buf, &b, 1, 0) == 1) return b;
+        return -1;
+    }
+    
+    int appBufAvailable() override {
+        return xStreamBufferBytesAvailable(stream_buf);
+    }
+    
+    void clearAppBuf() override {
+        xStreamBufferReset(stream_buf);
+    }
 };
