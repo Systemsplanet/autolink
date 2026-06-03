56d87089d45ac9fbb7acdb0e2b2fff6b1dbdaf31
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-03 12:53:16 -0400

    gitupload: AutoLink.h

--- AutoLink.h
+++ AutoLink.h
@@ -11,17 +11,18 @@ private:
 
 public:
     AutoLink(uart_port_t u_num, int rx_pin, int tx_pin, bool isMasterNode, 
-             std::vector<int> allowedBauds = {9600, 19200, 38400, 57600, 115200}, 
+             const std::vector<int>& allowedBauds = {9600, 19200, 38400, 57600, 115200}, 
              int errThresh = 5, 
              int delayMs = 50) 
     {
         hal.reset(new EspHal(u_num, rx_pin, tx_pin));
-        link.reset(new ALink(hal.get(), isMasterNode, allowedBauds, errThresh, delayMs));
+        link.reset(new ALink(*hal, isMasterNode, allowedBauds, errThresh, delayMs));
     }
 
-    int available() { return link->available(); }
+    bool isHealthy() const { return hal->isHealthy(); }
+    int available() const { return link->available(); }
     int read(uint8_t* b, int max_len) { return link->read(b, max_len); }
     void write(const uint8_t* b, int len) { link->write(b, len); }
     void err() { link->err(); }
-    State getState() { return link->getState(); }
+    State getState() const { return link->getState(); }
 };
