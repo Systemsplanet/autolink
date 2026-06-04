009d79e32ff136806abfabbeec6c351dfb4945cc
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-03 22:21:26 -0400

    gitupload: ILink.h

--- ILink.h
+++ ILink.h
@@ -10,7 +10,7 @@ public:
     virtual ~ILink() {}
     void bind(ALink* l) { link = l; }
     
-    virtual void setSpd(int s) = 0;
+    virtual void setSpd(uint32_t s) = 0;
     virtual void sendBreak() = 0;
     virtual void tx(const uint8_t* b, int n) = 0;
     virtual void flushTx() = 0;
