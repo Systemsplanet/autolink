486afdedf896c8e2341917dd9b7f799cf4b28b86
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-05 00:34:20 -0400

    gitupload: ILink.h

--- ILink.h
+++ ILink.h
@@ -19,11 +19,9 @@ public:
     virtual void startTimer(int ms) = 0;
     virtual void stopTimer() = 0;
     
-    // Concurrency Controls
     virtual void lock() const = 0;
     virtual void unlock() const = 0;
     
-    // Abstract Buffering
     virtual void pushAppBuf(uint8_t b) = 0;
     virtual int popAppBuf() = 0;
     virtual int peekAppBuf() = 0;
