0538c6e46e938a93430f2416cb5a9ab6ff968a0c
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-09 02:23:47 -0400

    gitupload: EspHal.h

--- EspHal.h
+++ EspHal.h
@@ -5,6 +5,7 @@
 #include <Arduino.h>
 #include "driver/uart.h"
 #include "esp_err.h"
+#include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "freertos/timers.h"
