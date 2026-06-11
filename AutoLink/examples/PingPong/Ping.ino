8fca2851f96ccdb0c6b5ed93b1d82e82937be9af
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-11 19:32:59 -0400

    gitupload: AutoLink/examples/PingPong/Ping.ino

--- AutoLink/examples/PingPong/Ping.ino
+++ AutoLink/examples/PingPong/Ping.ino
@@ -10,7 +10,7 @@
 // (last three args) to enable the live web dashboard, or omit them for a
 // UART-only link.
 
-#include <UtilPing.h>
+#include <util/UtilPing.h>
 using namespace autolink;
 
 UtilPing ping(
