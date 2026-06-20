8d95cdd2d19d42c3587e1aeda98afd7d39ad99cb
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-19 23:12:36 -0400

    gitupload: AutoLink/src/AutoLink.h

--- AutoLink/src/AutoLink.h
+++ AutoLink/src/AutoLink.h
@@ -82,7 +82,7 @@ public:
 namespace autolink {
 
 // Keep in sync with library.properties.
-#define AUTOLINK_VERSION "5.1.33"
+#define AUTOLINK_VERSION "5.1.34"
 
 class AutoLink : public Stream {
 private:
