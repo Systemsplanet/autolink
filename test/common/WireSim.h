9ba96bd3c0724c29f6b6ef035e659ec2f337d914
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-25 11:24:50 -0400

    gitupload: test/common/WireSim.h

--- test/common/WireSim.h
+++ test/common/WireSim.h
@@ -1,11 +1,8 @@
-// Closed-loop wire simulator: two IHal adapters
-// talking to each other via two memory pipes; MockHal
-// drives the clock. Lets the host test suite run real
-// Link ↔ Link traffic with no hardware.
+// Closed-loop wire simulator:
+// two MockHal adapters + shared clock.
 #pragma once
 #ifndef AUTOLINK_HOST_TEST
-#    error \
-        "Build with -DAUTOLINK_HOST_TEST (see Makefile)"
+#    error "Build with -DAUTOLINK_HOST_TEST (see Makefile)"
 #endif
 
 #include "MockHal.h"
@@ -22,8 +19,7 @@ class WireSim
 {
 public:
     WireSim(AutoLinkConfig cfg = AutoLinkConfig())
-        : mA_(new MockHal()), mB_(new MockHal()),
-          a_(mA_.get(), true, cfg),
+        : mA_(new MockHal()), mB_(new MockHal()), a_(mA_.get(), true, cfg),
           b_(mB_.get(), false, cfg)
     {
         mA_->peer = mB_.get();
@@ -33,28 +29,20 @@ public:
         mB_->now = 0;
     }
 
-
     void setFrameDropPct(int pct)
     {
         mA_->frameDropPct = pct;
         mB_->frameDropPct = pct;
     }
 
-
-    void setForcedDropEvery(int n)
-    {
-        forcedDropEvery_ = n;
-    }
-
+    void setForcedDropEvery(int n) { forcedDropEvery_ = n; }
 
     void requestDrop() { dropPending_ = true; }
-
 
     MockHal &halA() { return *mA_; }
     MockHal &halB() { return *mB_; }
     AutoLink &linkA() { return a_; }
     AutoLink &linkB() { return b_; }
-
 
     void step(uint32_t deltaMs)
     {
@@ -62,18 +50,14 @@ public:
         a_.linkForTest()->getStats(preA);
         b_.linkForTest()->getStats(preB);
 
-
         if (dropPending_ ||
-            (forcedDropEvery_ > 0 &&
-             (++stepCount_) % forcedDropEvery_ == 0)) {
-            if (dropPending_ ||
-                (forcedDropEvery_ > 0)) {
+            (forcedDropEvery_ > 0 && (++stepCount_) % forcedDropEvery_ == 0)) {
+            if (dropPending_ || (forcedDropEvery_ > 0)) {
                 a_.dropLink();
                 dropsInjected_++;
                 dropPending_ = false;
             }
         }
-
 
         int fireCountA = 0, fireCountB = 0;
         mA_->now += deltaMs;
@@ -91,27 +75,23 @@ public:
             b_.linkForTest()->onTimer();
         }
 
-
         pipe_data(*mA_, *mB_);
         pipe_data(*mB_, *mA_);
 
         Stats postA, postB;
         a_.linkForTest()->getStats(postA);
         b_.linkForTest()->getStats(postB);
-        int protoDropsThisStep =
-            (int)((postA.discCount - preA.discCount) +
-                  (postB.discCount - preB.discCount));
+        int protoDropsThisStep = (int)((postA.discCount - preA.discCount) +
+                                       (postB.discCount - preB.discCount));
         if (protoDropsThisStep > 0)
             protoDropsSeen_ += protoDropsThisStep;
     }
-
 
     uint64_t bytesTransferredAtoB() const
     {
         Stats sa, sb;
         a_.linkForTest()->getStats(sa);
         b_.linkForTest()->getStats(sb);
-
 
         return sb.rx;
     }
@@ -123,33 +103,16 @@ public:
         return sa.rx;
     }
 
-    int dropsInjected() const
-    {
-        return dropsInjected_;
-    }
-    int protoDropsSeen() const
-    {
-        return protoDropsSeen_;
-    }
-    int framesDropped() const
-    {
-        return mA_->bytesDropped + mB_->bytesDropped;
-    }
-    int pendingCountA() const
-    {
-        return a_.arqCacheSizeForTest();
-    }
-    int pendingCountB() const
-    {
-        return b_.arqCacheSizeForTest();
-    }
+    int dropsInjected() const { return dropsInjected_; }
+    int protoDropsSeen() const { return protoDropsSeen_; }
+    int framesDropped() const { return mA_->bytesDropped + mB_->bytesDropped; }
+    int pendingCountA() const { return a_.arqCacheSizeForTest(); }
+    int pendingCountB() const { return b_.arqCacheSizeForTest(); }
     State getStateA() const { return a_.getState(); }
     State getStateB() const { return b_.getState(); }
 
-
     AutoLink &nodeAForTest() { return a_; }
     AutoLink &nodeBForTest() { return b_; }
-
 
     const MockHal &rawA() const { return *mA_; }
     const MockHal &rawB() const { return *mB_; }
@@ -165,7 +128,6 @@ private:
     bool dropPending_ = false;
     int dropsInjected_ = 0;
 
-
     uint64_t lastDiscA_ = 0;
     uint64_t lastDiscB_ = 0;
     int protoDropsSeen_ = 0;
@@ -180,10 +142,8 @@ public:
         pendingB_.assign(32, Slot{ false, 0, 0, 0 });
     }
 
-
     AutoLink &nodeA() { return sim_.nodeAForTest(); }
     AutoLink &nodeB() { return sim_.nodeBForTest(); }
-
 
     void begin()
     {
@@ -191,14 +151,11 @@ public:
         sim_.linkB().begin();
     }
 
-
     uint64_t step(uint32_t deltaMs)
     {
-        int dropsBefore = sim_.dropsInjected() +
-            sim_.protoDropsSeen();
+        int dropsBefore = sim_.dropsInjected() + sim_.protoDropsSeen();
         sim_.step(deltaMs);
-        int dropsAfter = sim_.dropsInjected() +
-            sim_.protoDropsSeen();
+        int dropsAfter = sim_.dropsInjected() + sim_.protoDropsSeen();
         if (dropsAfter > dropsBefore) {
             for (auto &s : pendingA_)
                 s.active = false;
@@ -206,19 +163,13 @@ public:
                 s.active = false;
         }
 
-
         drivePing_();
 
         drivePong_();
         return sim_.bytesTransferredAtoB();
     }
 
-
-    uint64_t totalBytesAtoB() const
-    {
-        return sim_.bytesTransferredAtoB();
-    }
-
+    uint64_t totalBytesAtoB() const { return sim_.bytesTransferredAtoB(); }
 
     WireSim &sim() { return sim_; }
     const MockHal &halA() { return sim_.rawA(); }
@@ -233,18 +184,10 @@ public:
         sim_.halB().now;
         return sim_.pendingCountB();
     }
-    State getStateA() const
-    {
-        return sim_.getStateA();
-    }
-    State getStateB() const
-    {
-        return sim_.getStateB();
-    }
-
+    State getStateA() const { return sim_.getStateA(); }
+    State getStateB() const { return sim_.getStateB(); }
 
     int maxBurstPerLoop = 4;
-
 
     int msgSize = 64;
     int windowSize = 32;
@@ -275,7 +218,6 @@ private:
             buf[i] = nextByte_();
     }
 
-
     void drivePing_()
     {
         if (sim_.getStateA() != State::OK)
@@ -283,8 +225,7 @@ private:
 
         uint8_t buf[1024];
         int n;
-        while ((n = sim_.linkA().recvMsg(
-                    buf, sizeof buf)) > 0) {
+        while ((n = sim_.linkA().recvMsg(buf, sizeof buf)) > 0) {
             for (auto &s : pendingA_) {
                 if (s.active && s.len == n) {
                     s.active = false;
@@ -292,7 +233,6 @@ private:
                 }
             }
         }
-
 
         int sent = 0;
         for (auto &s : pendingA_) {
@@ -304,13 +244,10 @@ private:
             fillPayload_(buf, len);
             bool ok = sim_.linkA().sendMsg(buf, len);
             if (debug_log_) {
-                std::cerr << "[drivePing_] sendMsg("
-                          << len << ") -> "
+                std::cerr << "[drivePing_] sendMsg(" << len << ") -> "
                           << (ok ? "true" : "false")
-                          << " stateA="
-                          << (int)sim_.getStateA()
-                          << " facadeCache="
-                          << sim_.pendingCountA()
+                          << " stateA=" << (int)sim_.getStateA()
+                          << " facadeCache=" << sim_.pendingCountA()
                           << std::endl;
             }
             if (ok) {
@@ -324,13 +261,10 @@ private:
             }
         }
         if (debug_log_) {
-            std::cerr
-                << "[drivePing_] sent=" << sent
-                << " pendingA_=" << pendingA_.size()
-                << std::endl;
+            std::cerr << "[drivePing_] sent=" << sent
+                      << " pendingA_=" << pendingA_.size() << std::endl;
         }
     }
-
 
     void drivePong_()
     {
@@ -339,8 +273,7 @@ private:
         uint8_t buf[1024];
         int n;
         int sent = 0;
-        while ((n = sim_.linkB().recvMsg(
-                    buf, sizeof buf)) > 0 &&
+        while ((n = sim_.linkB().recvMsg(buf, sizeof buf)) > 0 &&
                sent < maxBurstPerLoop) {
             if (sim_.linkB().sendMsg(buf, n)) {
                 sent++;
