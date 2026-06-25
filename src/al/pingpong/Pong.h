6ccf0443f661d7af835d068ff4da34869818949f
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-25 11:29:55 -0400

    gitupload: src/al/pingpong/Pong.h

--- src/al/pingpong/Pong.h
+++ src/al/pingpong/Pong.h
@@ -5,31 +5,29 @@
 
 #    include "al/pingpong/PingPongBase.h"
 
-namespace autolink
-{
-class Pong
-{
+namespace autolink {
+class Pong {
 public:
     Pong(uint32_t debugBaud, uart_port_t uartNum, int rxPin, int txPin,
          const char *ssid = nullptr, const char *password = nullptr,
          uint16_t webPort = 8765)
         : base_(debugBaud, uartNum, rxPin, txPin, false, ssid, password,
-                webPort)
-    {
-    }
+                webPort) {}
 
     Pong(const Pong &) = delete;
     Pong &operator=(const Pong &) = delete;
 
-    void setup()
-    {
-        base_.log_.debug("Pong", "setup: calling setupCommon");
-        base_.setupCommon();
+    void setup() {
+        base_.log_.debug("Pong", "setup: sequencing init steps");
+        const char *role = "Pong";
+        initSerial(base_.log_, base_.debugBaud_, role, base_.ssid_);
+        bringUpLink(base_.log_, base_.comm_);
+        startWebMonitor(base_.log_, base_.mon_, role, base_.ssid_,
+                        base_.password_, base_.webPort_);
         base_.log_.info("Pong", "mode=Pong  ready");
     }
 
-    void loop()
-    {
+    void loop() {
         if (!base_.comm_.ready()) {
             if (base_.wasReady_) {
                 base_.log_.info("Pong", "link lost  echoes_sent=%lu",
@@ -37,7 +35,7 @@ public:
                 base_.wasReady_ = false;
                 tNotReady_ = millis();
 
-                base_.resetStatBaseline();
+                resetStatBaseline(stat_);
             } else {
                 uint32_t now = millis();
                 if (now - tNotReady_ >= 1000) {
@@ -66,7 +64,7 @@ public:
         int recvThisLoop = 0;
         const int maxEcho = (base_.comm_.mode() == AutoLinkConfig::Mode::SYNC)
             ? 1
-            : MAX_TX_PER_LOOP;
+            : PingPongBase::MAX_TX_PER_LOOP;
         while ((n = base_.comm_.recv(base_.buf_, sizeof base_.buf_)) > 0 &&
                recvThisLoop < maxEcho) {
             recvThisLoop++;
@@ -87,14 +85,13 @@ public:
                              (unsigned long)echoCount_);
         }
 
-        base_.logStats("Pong", echoCount_, 0);
+        logStats(base_.log_, "Pong", base_.comm_, stat_, echoCount_, 0);
     }
 
 private:
     uint64_t echoCount_ = 0;
     uint32_t tNotReady_ = 0;
-
-    static constexpr int MAX_TX_PER_LOOP = 16;
+    StatBaseline stat_;
 
     PingPongBase base_;
 };
