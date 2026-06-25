f2393110a7c4c272e3a830bb92b34972aba38528
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-25 11:41:28 -0400

    gitupload: src/al/link/sweep/LinkBaudSweep.cpp

---
+++ src/al/link/sweep/LinkBaudSweep.cpp
@@ -0,0 +1,32 @@
+// Per-baud PONG_ACK accumulator.
+#include "al/link/sweep/LinkBaudSweep.h"
+
+namespace autolink {
+void UtilBaudSweep::configure(const Config &c) {
+    cfg_ = c;
+    if (cfg_.expectedSamples < 0)
+        cfg_.expectedSamples = cfg_.pingSamplesPerBaud;
+    if (cfg_.expectedSamples < 1)
+        cfg_.expectedSamples = 1;
+}
+
+void UtilBaudSweep::resetAll() {
+    for (int i = 0; i < numBauds_; i++)
+        scores_[i] = 0;
+}
+
+int UtilBaudSweep::pickBest() const {
+    int minHits = (int)(cfg_.minAcceptRate * (float)cfg_.expectedSamples);
+    if (minHits < 1)
+        minHits = 1;
+    for (int j = 0; j < numBauds_; j++)
+        if (scores_[j] >= minHits)
+            return j;
+    // Fall back to any baud with a hit.
+    for (int j = 0; j < numBauds_; j++)
+        if (scores_[j] > 0)
+            return j;
+    return -1;
+}
+
+} // namespace autolink
