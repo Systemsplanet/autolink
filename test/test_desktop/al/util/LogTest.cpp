5335b818b297321ec977620f2c205a461096333d
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-25 11:24:11 -0400

    gitupload: test/test_desktop/al/util/LogTest.cpp

--- test/test_desktop/al/util/LogTest.cpp
+++ test/test_desktop/al/util/LogTest.cpp
@@ -1,5 +1,4 @@
-// Log sink fan-out, level filter, truncation warning.
-
+// Log sink fan-out, level filter.
 #ifndef ARDUINO
 
 #    include <iostream>
@@ -17,22 +16,18 @@ struct CapturedLine {
     std::string msg;
 };
 static std::vector<CapturedLine> g_captured;
-static void captureSink(char sev, const char *tag,
-                        const char *msg, void *)
+static void captureSink(char sev, const char *tag, const char *msg, void *)
 {
-    g_captured.push_back(
-        { sev, tag ? tag : "", msg ? msg : "" });
+    g_captured.push_back({ sev, tag ? tag : "", msg ? msg : "" });
 }
 
 void test_level_filtering()
 {
-    std::cout << "\n=== Test: Log Level Filtering (4: "
-                 "NONE..DEBUG) ==="
+    std::cout << "\n=== Test: Log Level Filtering (4: NONE..DEBUG) ==="
               << std::endl;
     Log &L = Log::log();
     L.setSink(captureSink);
     g_captured.clear();
-
 
     L.setLevel(Log::NONE);
     L.error("T", "err_should_not_appear");
@@ -45,8 +40,7 @@ void test_level_filtering()
     L.info("T", "info1");
     L.debug("T", "dbg1");
     assert(g_captured.size() == 1);
-    assert(g_captured[0].sev == 'E' &&
-           g_captured[0].msg == "err1");
+    assert(g_captured[0].sev == 'E' && g_captured[0].msg == "err1");
 
     L.setLevel(Log::WARNING);
     L.error("T", "err2");
@@ -54,10 +48,8 @@ void test_level_filtering()
     L.info("T", "info2");
     L.debug("T", "dbg2");
     assert(g_captured.size() == 3);
-    assert(g_captured[1].sev == 'E' &&
-           g_captured[1].msg == "err2");
-    assert(g_captured[2].sev == 'W' &&
-           g_captured[2].msg == "warn2");
+    assert(g_captured[1].sev == 'E' && g_captured[1].msg == "err2");
+    assert(g_captured[2].sev == 'W' && g_captured[2].msg == "warn2");
 
     L.setLevel(Log::INFO);
     L.error("T", "err3");
@@ -65,10 +57,8 @@ void test_level_filtering()
     L.debug("T", "dbg3");
 
     assert(g_captured.size() == 5);
-    assert(g_captured[3].sev == 'E' &&
-           g_captured[3].msg == "err3");
-    assert(g_captured[4].sev == 'I' &&
-           g_captured[4].msg == "info3");
+    assert(g_captured[3].sev == 'E' && g_captured[3].msg == "err3");
+    assert(g_captured[4].sev == 'I' && g_captured[4].msg == "info3");
 
     L.setLevel(Log::DEBUG);
     L.error("T", "err4");
@@ -76,13 +66,9 @@ void test_level_filtering()
     L.debug("T", "dbg4");
 
     assert(g_captured.size() == 8);
-    assert(g_captured[5].sev == 'E' &&
-           g_captured[5].msg == "err4");
-    assert(g_captured[6].sev == 'I' &&
-           g_captured[6].msg == "info4");
-    assert(g_captured[7].sev == 'D' &&
-           g_captured[7].msg == "dbg4");
-
+    assert(g_captured[5].sev == 'E' && g_captured[5].msg == "err4");
+    assert(g_captured[6].sev == 'I' && g_captured[6].msg == "info4");
+    assert(g_captured[7].sev == 'D' && g_captured[7].msg == "dbg4");
 
     L.setLevel(Log::VERBOSE);
     L.error("T", "err5");
@@ -91,21 +77,16 @@ void test_level_filtering()
     L.verbose("T", "vb5");
 
     assert(g_captured.size() == 12);
-    assert(g_captured[8].sev == 'E' &&
-           g_captured[8].msg == "err5");
-    assert(g_captured[9].sev == 'I' &&
-           g_captured[9].msg == "info5");
-    assert(g_captured[10].sev == 'D' &&
-           g_captured[10].msg == "dbg5");
-    assert(g_captured[11].sev == 'V' &&
-           g_captured[11].msg == "vb5");
+    assert(g_captured[8].sev == 'E' && g_captured[8].msg == "err5");
+    assert(g_captured[9].sev == 'I' && g_captured[9].msg == "info5");
+    assert(g_captured[10].sev == 'D' && g_captured[10].msg == "dbg5");
+    assert(g_captured[11].sev == 'V' && g_captured[11].msg == "vb5");
 
     assert((int)Log::NONE < (int)Log::ERROR);
     assert((int)Log::ERROR < (int)Log::WARNING);
     assert((int)Log::WARNING < (int)Log::INFO);
     assert((int)Log::INFO < (int)Log::DEBUG);
     assert((int)Log::DEBUG < (int)Log::VERBOSE);
-
 
     L.setLevel(Log::WARNING);
     assert(L.wouldEmit(Log::ERROR));
@@ -136,9 +117,7 @@ void test_level_filtering()
 
 void test_sink_registration_and_clearing()
 {
-    std::cout << "\n=== Test: Sink Registration & "
-                 "Clearing ==="
-              << std::endl;
+    std::cout << "\n=== Test: Sink Registration & Clearing ===" << std::endl;
     Log &L = Log::log();
     L.setLevel(Log::DEBUG);
     L.setSink(captureSink);
@@ -158,13 +137,11 @@ void test_sink_registration_and_clearing()
 
 void test_sink_context_pointer_passes_through()
 {
-    std::cout << "\n=== Test: Sink Context Pointer ==="
-              << std::endl;
+    std::cout << "\n=== Test: Sink Context Pointer ===" << std::endl;
     Log &L = Log::log();
     L.setLevel(Log::DEBUG);
     int marker = 0xCAFE;
-    auto fn = [](char, const char *, const char *,
-                 void *ctx) {
+    auto fn = [](char, const char *, const char *, void *ctx) {
         int *m = reinterpret_cast<int *>(ctx);
         *m = 0xBEEF;
     };
@@ -177,9 +154,7 @@ void test_sink_context_pointer_passes_through()
 
 void test_sink_called_within_emit()
 {
-    std::cout
-        << "\n=== Test: Sink Called Within emit() ==="
-        << std::endl;
+    std::cout << "\n=== Test: Sink Called Within emit() ===" << std::endl;
     Log &L = Log::log();
     L.setLevel(Log::DEBUG);
     L.setSink(captureSink);
@@ -195,14 +170,11 @@ void test_sink_called_within_emit()
 
 void test_long_message_truncated_at_buffer()
 {
-    std::cout
-        << "\n=== Test: Long Message Truncated ==="
-        << std::endl;
+    std::cout << "\n=== Test: Long Message Truncated ===" << std::endl;
     Log &L = Log::log();
     L.setLevel(Log::DEBUG);
     L.setSink(captureSink);
     g_captured.clear();
-
 
     std::string big(1024, 'A');
     L.info("T", "%s", big.c_str());
@@ -215,9 +187,7 @@ void test_long_message_truncated_at_buffer()
 
 void test_singleton_returns_same_instance()
 {
-    std::cout
-        << "\n=== Test: Log::log() Is Singleton ==="
-        << std::endl;
+    std::cout << "\n=== Test: Log::log() Is Singleton ===" << std::endl;
     Log &a = Log::log();
     Log &b = Log::log();
     assert(&a == &b);
@@ -226,17 +196,14 @@ void test_singleton_returns_same_instance()
 
 int main()
 {
-    std::cout << "=== Running Log Tests ==="
-              << std::endl;
+    std::cout << "=== Running Log Tests ===" << std::endl;
     test_level_filtering();
     test_sink_registration_and_clearing();
     test_sink_context_pointer_passes_through();
     test_sink_called_within_emit();
     test_long_message_truncated_at_buffer();
     test_singleton_returns_same_instance();
-    std::cout
-        << "\n=== Log Tests Completed Successfully ==="
-        << std::endl;
+    std::cout << "\n=== Log Tests Completed Successfully ===" << std::endl;
     return 0;
 }
 
