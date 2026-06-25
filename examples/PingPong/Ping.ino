b00116a689817e66a71dffed9380cda884a05812
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-25 11:27:33 -0400

    gitupload: examples/PingPong/Ping.ino

--- examples/PingPong/Ping.ino
+++ examples/PingPong/Ping.ino
@@ -22,11 +22,5 @@ PingPong upp(PingPong::PING,
              80            // web server port
 );
 
-void setup()
-{
-    upp.setup();
-}
-void loop()
-{
-    upp.loop();
-}
+void setup() { upp.setup(); }
+void loop() { upp.loop(); }
