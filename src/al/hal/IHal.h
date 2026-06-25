acda51a1fa38f3acaec2c62261fa8a255beaf49d
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-25 11:41:07 -0400

    gitupload: src/al/hal/IHal.h

---
+++ src/al/hal/IHal.h
@@ -0,0 +1,43 @@
+// HAL boundary: Link talks only to IHal.
+// EspHal implements on hardware; MockHal
+// implements with memory pipes for host tests.
+#pragma once
+#include <stdint.h>
+
+namespace autolink {
+class Link;
+
+class IHal {
+public:
+    Link *link = nullptr;
+
+    virtual ~IHal() {}
+    void bind(Link *l) { link = l; }
+
+    virtual bool isHealthy() const { return true; }
+
+    virtual void begin() = 0;
+    virtual void setSpd(uint32_t s) = 0;
+    virtual void sendBreak() = 0;
+
+    virtual int tx(const uint8_t *b, int n) = 0;
+    virtual void flushTx() = 0;
+    virtual void startTimer(int ms) = 0;
+    virtual void stopTimer() = 0;
+    virtual void delayMs(int ms) = 0;
+    virtual uint32_t nowMs() = 0;
+
+    virtual void lock() = 0;
+    virtual void unlock() = 0;
+
+    virtual int pushAppBuf(const uint8_t *b, int n) = 0;
+    virtual int popAppBuf(uint8_t *b, int max_len) = 0;
+    virtual int peekAppBuf() const = 0;
+    virtual int appBufAvailable() const = 0;
+    virtual int peekAt(uint8_t *out, int n, int offset) const = 0;
+    virtual void clearAppBuf() = 0;
+
+    virtual void flushRxHw() {}
+};
+
+} // namespace autolink
