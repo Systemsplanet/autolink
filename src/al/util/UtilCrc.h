e2e8c8712f81e3f3b7f8563fbaf3cb19eab64a4c
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-06-27 20:09:50 -0400

    gitupload: src/al/util/UtilCrc.h

---
+++ src/al/util/UtilCrc.h
@@ -0,0 +1,17 @@
+// CRC-8 (control/ACK frames) and
+// CRC-16 (MSG_HDR + payload) with LUTs.
+#pragma once
+#include <stdint.h>
+
+namespace autolink {
+class UtilCrc {
+public:
+    static uint8_t crc8(const uint8_t *data, int len);
+    static uint16_t crc16(const uint8_t *data, int len);
+
+private:
+    static const uint8_t crc8_lut[256];
+    static const uint16_t crc16_lut[256];
+};
+
+} // namespace autolink
