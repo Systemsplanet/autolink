c4c3f45dd316399c306626b1d5fc005c7e580f5f
Systemsplanet <132013087+Systemsplanet@users.noreply.github.com>
2026-07-21 17:36:07 -0400

    gitupload: src/al/util/UtilCobs.h

---
+++ src/al/util/UtilCobs.h
@@ -0,0 +1,16 @@
+
+#pragma once
+#include <stdint.h>
+#include <stddef.h>
+
+namespace autolink {
+class UtilCobs {
+public:
+    static size_t encodedMax(size_t n) { return n + n / 254 + 1; }
+
+    static size_t encode(const uint8_t *src, size_t len, uint8_t *dst);
+
+    static size_t decode(const uint8_t *src, size_t len, uint8_t *dst);
+};
+
+} // namespace autolink
